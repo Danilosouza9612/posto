22aa83040d54694be3490619216ad27ce81981e8
Your Name <you@example.com>
2019-10-29 23:35:58 -0300

    fazendo telas

--- Posto/src/main/webapp/frontend/src/app/App.js
+++ Posto/src/main/webapp/frontend/src/app/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import AsideBtn from './asideBtn';
-import NovoAbastecimento from './mainComponents/abastecer';
+import NovoAbastecimento from './mainComponents/abastecertela/abastecer';
 import Inicio from './mainComponents/iniciotela/inicio';
+import AbastecerBomba from "./mainComponents/abastecerbombatela/abastecerbomba";
+import BombasTabela from "./mainComponents/bombastela/bombastabela";
+import QtdAbastecimento from "./mainComponents/qtdabastecimentotela/qtdabastecimento";
+import ClienteAssiduo from "./mainComponents/clienteassiduotela/clienteassiduo";
 import './App.css';
 import './mainComponents/main.css';
 
@@ -24,11 +28,11 @@ class App extends React.Component {
           <div>
             <AsideBtn text="Início" onClick={()=>this.setState({mainState : <Inicio/>})}/>
             <AsideBtn text="Abastecer" onClick={()=>this.setState({mainState : <NovoAbastecimento/>})}/>
-            <AsideBtn text="Abastecer Bomba"/>
+            <AsideBtn text="Abastecer Bomba" onClick={()=>this.setState({mainState : <AbastecerBomba/>})} />
             <AsideBtn text="Levantar Faturamento"/>
-            <AsideBtn text="Bombas de combustível"/>
-            <AsideBtn text="Abastecimentos"/>
-            <AsideBtn text="Clientes mais assíduos"/>
+            <AsideBtn text="Bombas de combustível" onClick={()=>this.setState({mainState : <BombasTabela/>})}/>
+            <AsideBtn text="Abastecimentos" onClick={()=>this.setState({mainState: <QtdAbastecimento/>})}/>
+            <AsideBtn text="Clientes mais assíduos" onClick={()=>this.setState({mainState : <ClienteAssiduo/>})}/>
           </div>
         </aside>
         <main>
--- Posto/src/main/webapp/frontend/src/app/mainComponents/component.js
+++ Posto/src/main/webapp/frontend/src/app/mainComponents/component.js
@@ -4,18 +4,20 @@ export const InputComponent = function(props){
     return (
         <div>
             <label for={props.name}>{props.text}</label>
-            <input type={props.type} name={props.name} id={props.name} value={props.value}></input>
+            <input type={props.type} name={props.name} id={props.name} value={props.value} onChange={props.onChange}></input>
         </div>
     );
 }
 export const OptionComponent = function(props){
-    const options = props.options.map((op) => <option value={op.id}>{op.nome}</option>)
+    const opcoes = props.options.map((op) => {
+        return <option value={op.id}>{op.nome}</option>
+    });
 
     return (
         <div>
             <label for={props.name}>{props.text}</label>
-            <select name={props.name} id={props.name}>
-                {options}
+            <select name={props.name} id={props.name} onChange={props.onChange}>
+                {opcoes}
             </select>
         </div>
     );
